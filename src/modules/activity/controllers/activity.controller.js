7437b0787422c748e143155412de45562cbbe713
Omnia Farouk <omniafarouk2003@gmail.com>
2026-09-21 20:05:58 +0300

    changed activity controller function into async/await too

--- src/modules/activity/controllers/activity.controller.js
+++ src/modules/activity/controllers/activity.controller.js
@@ -1,14 +1,14 @@
 const aSvc = require('../services/activity.service');
 
-function get_activity(req, res) {
-  const x = aSvc.getAllActivity();
+async function get_activity(req, res) {
+  const x = await aSvc.getAllActivity();
   res.json(x);
 }
 
-function addActivity(req, res) {
+async function addActivity(req, res) {
   const bodyData = req.body || {};
   // Must add validation for the body here (body mush have info and action fields)
-  const made = aSvc.createNewActivity(bodyData);
+  const made = await aSvc.createNewActivity(bodyData);
   res.status(201).json(made);
 }
 
