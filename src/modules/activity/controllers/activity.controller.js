4c51627bbdc5e8b147a76912a736d999ef9ff441
Omnia Farouk <omniafarouk2003@gmail.com>
2026-09-21 20:07:59 +0300

    changed json response into consistent 'data' wrapper

--- src/modules/activity/controllers/activity.controller.js
+++ src/modules/activity/controllers/activity.controller.js
@@ -2,14 +2,14 @@ const aSvc = require('../services/activity.service');
 
 async function get_activity(req, res) {
   const x = await aSvc.getAllActivity();
-  res.json(x);
+  res.json({ data: x });
 }
 
 async function addActivity(req, res) {
   const bodyData = req.body || {};
   // Must add validation for the body here (body mush have info and action fields)
   const made = await aSvc.createNewActivity(bodyData);
-  res.status(201).json(made);
+  res.status(201).json({ data: made });
 }
 
 module.exports = {
